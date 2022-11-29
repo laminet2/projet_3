5643988313ca568bc88a5e8cef5b620562d65220
laminet2 <kamiya.traore@gmail.com>
2022-11-29 02:32:55 +0000

    Version 1.2

--- model/pret.model.php
+++ model/pret.model.php
@@ -9,12 +9,14 @@
          ];
          return $rayons;
     }
-    
+    //on affiche pas l'exemplaire mais l'ouvrage,
+    //un ouvrage est disponible s' il a au moins un exemplaire qui n'est pas en prêt ou détérioré
+
     function find_all_ouvrages():array{
         $ouvrages=[["id"=>1,"titre"=>"statistique et probabilité","date_edition"=>"mars 1997","mot_cle"=>["statistique","probabilite","khie-deux","student","loie"],"rayon_id"=>5],
                   ["id"=>2,"titre"=>"Maths pratiques terminale D","date_edition"=>"2019","mot_cle"=>["maths","terminale","denombrement","geometrie"],"rayon_id"=>5],
                   ["id"=>3,"titre"=>"le jeûne de ramadan ethique & preceptes ","date_edition"=>"fevrier 2017","mot_cle"=>["jeune","ramadan","ethique","preceptes"],"rayon_id"=>4],
-                  ["id"=>4 ,"titre"=>"Introduction au marketing","date_edition"=>"Octobre 2001","mot_cle"=>["marketing","introduction","initiation"],"rayon_id"=>2],
+                  ["id"=>4,"titre"=>"Introduction au marketing","date_edition"=>"Octobre 2001","mot_cle"=>["marketing","introduction","initiation"],"rayon_id"=>2],
                   ["id"=>5,"titre"=>"Methode de communication ecrite et orale","date_edition"=>"2008","mot_cle"=>["ecrite","communication","orale"],"rayon_id"=>1],
                   ["id"=>6,"titre"=>"l'art subtile de s'en foutre","date_edition"=>"2016","mot_cle"=>["art","subtile","foutre"],"rayon_id"=>3] ,
                   ["id"=>7,"titre"=>"Comment se faire des amis","date_edition"=>"1981","mot_cle"=>["amis","influence","communication"],"rayon_id"=>3]   ,
@@ -44,7 +46,7 @@
                 return $auteurs;
     }
     
-
+    
     function find_all_ecrie():array{
             $ecries=[["id"=>1,"ouvrage_id"=>1,"auteur_id"=>10],
                     ["id"=>2,"ouvrage_id"=>1,"auteur_id"=>11],
@@ -67,7 +69,7 @@
     function find_all_exemplaire():array{
         $exemplaires=[["id"=>1,"date_enregistrement"=>"01/01/2020","etat"=>"neuf","ouvrage_id"=>1],
                     ["id"=>2,"date_enregistrement"=>"01/01/2020","etat"=>"neuf","ouvrage_id"=>1],
-                    ["id"=>3,"date_enregistrement"=>"20/05/2017","etat"=>"deteriore","ouvrage_id"=>1],
+                    ["id"=>3,"date_enregistrement"=>"20/05/2017","etat"=>"deteriore","ouvrage_id"=>1], // les tables avec l'attribut etat sont deterioirer ou perdu
                     ["id"=>4,"date_enregistrement"=>"12/08/2020","etat"=>"neuf","ouvrage_id"=>2],
                     ["id"=>5,"date_enregistrement"=>"12/08/2020","etat"=>"neuf","ouvrage_id"=>2],
                     ["id"=>6,"date_enregistrement"=>"12/12/2017","etat"=>"neuf","ouvrage_id"=>3],
@@ -102,20 +104,23 @@
         return $adherent; 
     }
     
+    
 
+    //une demande de pret sans statut est en attente
     function find_all_demande_de_pret():array{
-            $demande_de_pret=[["id"=>1,"adherent_id"=>1,"exemplaire_id"=>1,"statut"=>"accepter","date_pret"=>""],//accepter mais pas encore passer a la bibliotheque
+            $demande_de_pret=[["id"=>1,"adherent_id"=>1,"exemplaire_id"=>1,"statut"=>"accepter"],//accepter mais pas encore passer a la bibliotheque
                               ["id"=>2,"adherent_id"=>5,"exemplaire_id"=>5,"statut"=>"accepter","date_pret"=>"25/11/2022"],//accepter et passer a la bibliotheque
-                              ["id"=>3,"adherent_id"=>3,"exemplaire_id"=>4,"statut"=>"","date_pret"=>""],//en attente
-                              ["id"=>4,"adherent_id"=>4,"exemplaire_id"=>3,"statut"=>"rejeter","date_pret"=>""],//rejeter
+                              ["id"=>3,"adherent_id"=>3,"exemplaire_id"=>4],//en attente
+                              ["id"=>4,"adherent_id"=>4,"exemplaire_id"=>3,"statut"=>"rejeter"],//rejeter
                               ["id"=>5,"adherent_id"=>2,"exemplaire_id"=>5,"statut"=>"accepter","date_pret"=>"20/11/2022"],//accepter et passer en bibliotheque
-                              ["id"=>6,"adherent_id"=>6,"exemplaire_id"=>8,"statut"=>"accepter","date_pret"=>"20/11/2022"],//accepter et passer en bibliotheque
-
+                              ["id"=>6,"adherent_id"=>6,"exemplaire_id"=>8,"statut"=>"accepter","date_pret"=>"20/11/2022","date_r_reel"=>"4/12/2022"],//accepter et passer en bibliotheque et retourner
                             ];      
 
             return $demande_de_pret;
     }
-    
+    //une demande de prêt termine son execution et  passe la tâche a la table de prêt si elle a un statut:accepeter et a une date_de_pret;
+    //c'est a dire que si l'on veut plus d'informations sur l'etat du prêt ( "en cours ou retourner ") on passe par la table prêt;
+
     // function pret_accepter():array{
     //     $demande_de_pret=find_all_demande_de_pret();
     //     $prets=[];
@@ -133,6 +138,10 @@
     //     return $prets;
     // }
 
+    
+    //pret en cours = pret accepeter avec une date_pret,
+    //pret retourner= pret en cours avec une date_r_reel,
+
     function find_all_pret():array{
             $prets=[["id"=>1,"date_pret"=>"25/11/2022","date_r_prevue"=>"9/12/2022","date_r_reel"=>"","adherent_id"=>5],
                     ["id"=>2,"date_pret"=>"20/11/2022","date_r_prevue"=>"4/12/2022","date_r_reel"=>"","adherent_id"=>2],
@@ -141,5 +150,5 @@
             ];
             return $prets;
     }
-
+    // archiver une fonction consiste à la supprimer de la table exemplaire et de l'ajouter dans la table archiver
 ?>
